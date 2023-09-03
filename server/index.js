9ef55df5a1d1db1b9ace06cb08106ed618840e24
Abhishek-Rath <abhi.rath39@gmail.com>
2023-09-03 21:11:20 +0530

    Put credentials in .env file

--- server/index.js
+++ server/index.js
@@ -5,6 +5,8 @@ import cors from 'cors';
 import postRoutes from './routes/posts.js'
 
 const app = express();
+const dotenv = require("dotenv")
+dotenv.config()
 
 
 app.use(bodyParser.json({limit: "30mb", extended: true}));
@@ -12,7 +14,7 @@ app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
 
 app.use('/posts', postRoutes)
-const CONNECTION_URL = 'mongodb+srv://abhi:abhi123@cluster0.wvyxlie.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.wvyxlie.mongodb.net/?retryWrites=true&w=majority`
 
 const PORT = process.env.PORT || 5000
 
